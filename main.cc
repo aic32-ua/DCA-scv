eaf005db4dcaf5cab1738542cf2c5cfee674e457
Alejandro Ibarra Coves <aic32@alu.ua.es>
2023-12-20 16:10:30 +0100

    Error introducido

--- main.cc
+++ main.cc
@@ -5,8 +5,11 @@ int main(){
     cout << "Introduce un numero para comprobar si es par o impar:" << endl;
     int num;
     cin >> num;
-
-    if (num%2==0) {
+    
+    if(num<0){
+    	cout << "ERROR: Numero negativo" << endl; //fallo a encontrar con bisect
+    }
+    else if (num%2==0) {
         cout << "Es un numero par" << endl;
     } else {
         cout << "Es un numero impar" << endl;
